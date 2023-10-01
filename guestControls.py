c159f0071231a0476b8cf3954709604df22cf796
JArchbell-USF <jarchbell@usf.edu>
2023-10-01 08:06:42 -0400

    fix: important links fix

--- guestControls.py
+++ guestControls.py
@@ -7,74 +7,53 @@ def guestControlSettings():
     exitInput = 0
     while exitInput == 0:
         spacer()
-        header('Guest Controls')
+        header('Guest Controls: ')
         uInput = input("Would you like to view/change your user settings? (Y/N): ")
 
-        if uInput == 'N' or 'n':
+        if uInput.lower() == 'n':  # Simplified the input checking
             exitInput = 1
-        elif uInput == 'Y' or 'y':
+        elif uInput.lower() == 'y':
             exitSubInput = 0
             while exitSubInput == 0:
                 spacer()
-                print("Here are your current settings.")
-            
-                if globalVars.userSettingMarketingEmail == True:
-                    tempEmail = 'Enabled'
-                else:
-                    tempEmail = 'Disabled'
-
-                if globalVars.userSettingMarketingSMS == True:
-                    tempSMS = 'Enabled'
-                else:
-                    tempSMS = 'Disabled'
-            
-                if globalVars.userSettingAdvertisementTargeted == True:
-                    tempAdTarget = 'Enabled'
-                else:
-                    tempAdTarget = 'Disabled'
-            
-                if globalVars.userSettingLanguage == 0:
-                    tempLanguage = 'English'
-                elif globalVars.userSettingLanguage == 1:
-                    tempLanguage = 'Spanish'
-                else:
-                    tempLanguage = 'English'
-
-                print("(1)  Recieving inCollege Emails -        " + tempEmail)
-                print("(2)  Recieving inCollege Texts -         " + tempSMS)
-                print("(3)  Recieving Targeted Advertisement -  " + tempAdTarget)
-                print("(4)  Current Language Setting -          " + tempLanguage)
                 
+                # The function is now being used to both print and get the status dynamically
+                tempEmail, tempSMS, tempAdTarget, tempLanguage = getAndDisplaySettings()
+
                 uinput = input("Which setting would you like to toggle (Q to quit and return): ")
 
-                if uInput == '1':
-                    if globalVars.userSettingMarketingEmail == True:
-                        globalVars.userSettingMarketingEmail == False
-                    else:
-                        globalVars.userSettingMarketingEmail == True
-                elif uInput == '2':
-                    if globalVars.userSettingMarketingSMS == True:
-                        globalVars.userSettingMarketingSMS == False
-                    else:
-                        globalVars.userSettingMarketingSMS == True
-                elif uInput == '3':
-                    if globalVars.userSettingAdvertisementTargeted == True:
-                        globalVars.userSettingAdvertisementTargeted == False
-                    else:
-                        globalVars.userSettingAdvertisementTargeted == True
-                elif uInput == '4':
-                    if globalVars.userSettingLanguage == 0:
-                        globalVars.userSettingLanguage == 1
-                    else:
-                        globalVars.userSettingLanguage == 0
-                elif uInput == 'Q' or 'q':
+                # Input handling here...
+                # After changing a setting, the getAndDisplaySettings() function will be called again due to the loop.
+                
+                if uinput == '1':
+                    globalVars.userSettingMarketingEmail = not globalVars.userSettingMarketingEmail
+                elif uinput == '2':
+                    globalVars.userSettingMarketingSMS = not globalVars.userSettingMarketingSMS
+                elif uinput == '3':
+                    globalVars.userSettingAdvertisementTargeted = not globalVars.userSettingAdvertisementTargeted
+                elif uinput == '4':
+                    globalVars.userSettingLanguage ^= 1
+                elif uinput.lower() == 'q':
                     # Update database with new changes
-                    # Need to figure out how to make this more readable later
                     cursor.execute("UPDATE users SET marketingEmail = ?, marketingSMS = ?, adsTargeted = ?, language = ? WHERE username = ?",(globalVars.userSettingMarketingEmail, globalVars.userSettingMarketingSMS, globalVars.userSettingAdvertisementTargeted, globalVars.userSettingLanguage, globalVars.username,))
                     conn.commit()
                     
                     exitSubInput = 1
                 else:
                     print("Invalid Option Try Again")
-            
-            return
+                
+def getAndDisplaySettings():
+    """Function to get and display the current settings"""
+    print("Here are your current settings.")
+    
+    tempEmail = 'Enabled' if globalVars.userSettingMarketingEmail else 'Disabled'
+    tempSMS = 'Enabled' if globalVars.userSettingMarketingSMS else 'Disabled'
+    tempAdTarget = 'Enabled' if globalVars.userSettingAdvertisementTargeted else 'Disabled'
+    tempLanguage = 'English' if globalVars.userSettingLanguage == 0 else 'Spanish'
+    
+    print("(1)  Receiving inCollege Emails -        " + tempEmail)
+    print("(2)  Receiving inCollege Texts -         " + tempSMS)
+    print("(3)  Receiving Targeted Advertisement -  " + tempAdTarget)
+    print("(4)  Current Language Setting -          " + tempLanguage)
+    
+    return tempEmail, tempSMS, tempAdTarget, tempLanguage
--- important.py
+++ important.py
@@ -52,6 +52,9 @@ def importantLinks():
 
 
 def copyrightNotice():
+    spacer()
+    header('Copyright Notice: ')
+    
     crNoticeText1 = 'Copyright © 2023 TeamAlaska. All rights reserved.'
     crNoticeText2 = 'The content of this website, including text, graphics, images, and other material, is protected under applicable copyright laws and is the property of TeamAlaska. Unauthorized use and/or duplication of this material without express and written permission from TeamAlaska is strictly prohibited.'
     crNoticeText3 = 'For permission requests, write to the owner, addressed “Attention: Permissions Coordinator,” '
@@ -74,6 +77,8 @@ def copyrightNotice():
     return
 
 def accessibility():
+    spacer()
+    header('Accessibility')
     accText = 'At inCollege, we\'re committed to accessibility. We believe in making our applications and services accessible to all users, regardless of ability. Our goal is to provide an inclusive experience by implementing features that support individuals with diverse needs and disabilities. We continuously work to enhance our accessibility and adhere to many of the available standards and guidelines. If you encounter any issues or have suggestions for improvement, please contact us.'
     wrappedText = textwrap.fill(accText, width=80)
     print("")
@@ -82,6 +87,8 @@ def accessibility():
     return
 
 def userAgreement():
+    spacer()
+    header('User Agreement: ')
     uaText = 'By using the services provided by inCollege, you agree to be bound by the terms and conditions outlined herein. These terms are designed to ensure a safe, secure, and respectful use of our platform for all users. Users must not exploit, misuse, or engage in activities that violate any applicable laws or regulations while using our services. inCollege reserves the right to modify these terms at any time, with changes effective immediately upon posting to our site. Your continued use of our services following any modifications signifies your acceptance of the updated terms. For any queries or concerns regarding this agreement, please contact us.'
     wrappedText = textwrap.fill(uaText, width=80)
     print("")
@@ -90,6 +97,8 @@ def userAgreement():
     return
 
 def privacyPolicy():
+    spacer()
+    header('Privacy Policy: ')
     ppText = 'inCollege values your privacy. We are committed to safeguarding the privacy of our website visitors and service users. Our policy outlines the type of personal information we collect, how we use it, and the measures we take to secure it. We only collect information necessary for delivering our services and enhancing user experience. Under no circumstances will your data be sold or shared with unauthorized third parties without your consent. Please be aware that our privacy practices may be amended over time as we adapt to new regulatory requirements. For detailed information and updates, kindly contact us.'
     wrappedText = textwrap.fill(ppText, width=80)
     print("")
@@ -101,6 +110,8 @@ def privacyPolicy():
         return
 
 def cookiePolicy():
+    spacer()
+    header('Cookie Policy: ')
     cpText = 'inCollege uses cookies to enhance your experience while navigating through our website. Cookies are small data files placed on your device that allow us to collect information on how you interact with our services, enabling us to optimize functionality, improve performance, and tailor content to your preferences. By using our site, you consent to the use of cookies in accordance with this policy. For more information on the types and purposes of cookies we use, or to adjust your cookie preferences, please contact us.'
     wrappedText = textwrap.fill(cpText, width=80)
     print("")
@@ -109,6 +120,8 @@ def cookiePolicy():
     return
 
 def copyrightPolicy():
+    spacer()
+    header('Copyright Policy: ')
     crpText = 'All content, including text, graphics, logos, images, and software, appearing on inCollege\'s website is the exclusive property of inCollege or its content suppliers and is protected by international copyright laws. Unauthorized use, reproduction, or distribution of this content without the express written consent of inCollege is strictly prohibited. For inquiries about obtaining permission to use any content on our site or for any copyright-related questions, please contact us.'
     wrappedText = textwrap.fill(crpText, width=80)
     print("")
@@ -117,6 +130,8 @@ def copyrightPolicy():
     return
 
 def brandPolicy():
+    spacer()
+    header('Brand Policy:')
     bpText = 'inCollege\'s logos, trademarks, icons, and other intellectual property (“Brand Assets”) are valuable assets that represent our company and products. These Brand Assets should be used in a manner that is consistent with our brand identity, values, and legal guidelines. Unauthorized use, modification, or manipulation of any of inCollege\'s Brand Assets is strictly prohibited. Users and partners wishing to use or refer to inCollege\'s Brand Assets for any public or commercial purpose must obtain written approval from inCollege beforehand. For more information, guidelines, or requests related to our Brand Policy, please contact us.'
     wrappedText = textwrap.fill(bpText, width=80)
     print("")
@@ -132,16 +147,15 @@ def Languages():
             tempLanguage = 'Spanish'
         else:
             tempLanguage = 'English'
+        spacer()
+        header('Language Setting')
         print("Your current language is set to " + tempLanguage)
         print("Only English and Spanish are supported at this time.")
         uInput = input("Would you like to change your language setting? (Y/N): ")
-        if uInput == 'N' or 'n':
+        if uInput == 'N' or uInput == 'n':
             return
-        if uInput == 'Y' or 'y':
-            if globalVars.userSettingLanguage == 0:
-                globalVars.userSettingLanguage == 1
-            else:
-                globalVars.userSettingLanguage == 0
+        if uInput == 'Y' or uInput == 'y':
+            globalVars.userSettingLanguage ^= 1
             cursor.execute("UPDATE users SET language = ? WHERE username = ?",(globalVars.userSettingLanguage, globalVars.username,))
             conn.commit()
             return
