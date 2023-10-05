38dec6fedb736d4fb46f7bc689bfadd69a74bef0
Jordan Archbell <jarchbell@usf.edu>
2023-10-05 08:51:49 -0400

    Add userSearchUni

--- userSearch.py
+++ userSearch.py
@@ -2,6 +2,8 @@ import time
 from UI import *
 from database import *
 
+foundUserID = None
+
 def userSearch(): 
     menuLooper = True
     while menuLooper:
@@ -17,7 +19,7 @@ def userSearch():
         if uInput == '1':
             userSearchName()
         elif uInput == '2':
-            print() # build and replace with userSearchUni
+            userSearchUni()
         elif uInput == '3':
             print() # build and replace with userSearchMajor
         elif uInput == 'Q' or uInput == 'q':
@@ -32,9 +34,9 @@ def userSearchName():
     firstName = input("\nEnter a first name to search: ")
     lastName = input("Enter a last name: ")
     
-    cursor.execute("SELECT userID, firstName, LastName FROM users WHERE firstName=? AND lastName=?",(firstName, lastName))
+    cursor.execute("SELECT userID, firstName, LastName FROM users WHERE firstName=? AND lastName=? AND isDeleted=0",(firstName, lastName))
     user = cursor.fetchone()
-    
+    print("\n")
     header('Search Results')
     if not user:
         print("They are not yet an inCollege member.")  
@@ -47,3 +49,23 @@ def userSearchName():
             print("send request here")# replace with function call to DB to add request
 
     time.sleep(3)
+
+def userSearchUni():
+    spacer()
+    header('Search by University')
+    uniInput = input("Enter the acronym of your University (ie. USF): ").upper()
+
+    cursor.execute("SELECT userID, firstName, LastName, userMajor FROM users WHERE userUniversity=? and isDeleted=0", (uniInput,))
+    results = cursor.fetchall()
+    print("\n")
+    header('Search Results')
+    print("Results Found: ", len(results))
+    print("\n")
+
+    for row in results:
+        print("First Name:\t", row[1])
+        print("Last Name: \t", row[2])
+        print("Major:     \t", row[3])
+        print("\n")
+    
+    input("Press Enter to Continue...")
