99b9f91bf312d287cd04b38895efdc6540b0f8b4
h-afreen <humayra289@gmail.com>
2023-10-20 21:31:54 -0400

    update to getFriends() so that users are able to go back to the previous menu

--- friendFunctions.py
+++ friendFunctions.py
@@ -73,7 +73,6 @@ def viewFriendRequests():
 
 
 def getFriends():
-    from loginLanding import userHome
     spacer()
     header('Your Network')
 
@@ -98,41 +97,47 @@ def getFriends():
                 #print(f"Press {counterNum+1} to View Profile:")
                 print("\n")
                 #counterNum = counterNum+1
+            while True: 
+                spacer()
+                print("Please select from the following menu options: ")
+                print("1. View a friend's profile")
+                print("2. Disconnect from a friend")
+                print ("3. Return to previous menu ")
+                choice = input("Please enter (1/2/3): ")
 
-            print("Please select from the following menu options: ")
-            print("1. View a friend's profile")
-            print("2. Disconnect from a friend")
-            print ("3. Return to previous menu ")
-            choice = input("Please enter (1/2/3): ")
+                if choice == '1':
+                    #print("Please enter the Friend ID number to View Profile:")
+                    uInput = int(input("Please enter the Friend ID number to View Profile:"))
+                    for id in userFriendID:
+                        if uInput == id:
+                            showFriendProfile(id)
+                    #spacer()
+                    continue
 
-            if choice == '1':
-                #print("Please enter the Friend ID number to View Profile:")
-                uInput = int(input("Please enter the Friend ID number to View Profile:"))
-                for id in userFriendID:
-                    if uInput == id:
-                        showFriendProfile(id)
-
-            elif choice == '2': 
-                # Prompt user to disconnect from a friend
-                while True:
-                    userResponse = input("Enter the ID of a friend you want to disconnect from (or 'Q' to quit): ")
-                    if userResponse.upper() == 'Q':
-                        break
-                    try:
-                        selectedFriendID = int(userResponse)
-                        updateFriendDisconnect(selectedFriendID)
-                    except ValueError:
-                        print("Invalid input. Please enter a valid Friend ID or 'Q' to quit.")
-            
-            #elif (uInput == 'Q' ) or (uInput == 'q' ):
-            #   spacer()
+                elif choice == '2': 
+                    # Prompt user to disconnect from a friend
+                    while True:
+                        userResponse = input("Enter the ID of a friend you want to disconnect from (or 'Q' to quit): ")
+                        if userResponse.upper() == 'Q':
+                            break
+                        try:
+                            selectedFriendID = int(userResponse)
+                            updateFriendDisconnect(selectedFriendID)
+                        except ValueError:
+                            print("Invalid input. Please enter a valid Friend ID or 'Q' to quit.")
+                    continue
+                elif choice == '3' :
+                    spacer()
+                    return 
+                    
 
         else:
             print("No one is in your network at the moment.")
+        
     except Exception as e:
         print(f"Error occurred while fetching friends: {e}")
 
-    #input("Press Enter to Continue...")
+
 
 ###########################################Epic-5 FUNCTIONS###############################################
 
@@ -141,56 +146,60 @@ def showFriendProfile(friendID):
     spacer()
     header('Friend Profile')
 
-    # Query the database to retrieve and display the friend's profile details based on their User ID        
-    #cursor.execute("SELECT * FROM users WHERE userID = ? AND marketingEmail = 1", (friendID,))
-    cursor.execute("SELECT * FROM profiles WHERE userID = ?", (friendID,))
-    friend_data = cursor.fetchone()
+    try: 
 
-    cursor.execute("SELECT * FROM users WHERE userID = ?", (friendID,))
-    user_name = cursor.fetchone()
+        # Query the database to retrieve and display the friend's profile details based on their User ID        
+        #cursor.execute("SELECT * FROM users WHERE userID = ? AND marketingEmail = 1", (friendID,))
+        cursor.execute("SELECT * FROM profiles WHERE userID = ?", (friendID,))
+        friend_data = cursor.fetchone()
 
-    header(
-        f"{user_name[3]} {user_name[4]}'s Profile:\n")
+        cursor.execute("SELECT * FROM users WHERE userID = ?", (friendID,))
+        user_name = cursor.fetchone()
 
-    if friend_data:
-        print(f"Title: {friend_data[2]}")
-        print(f"Major: {friend_data[3]}")
-        print(f"University: {friend_data[4]}")
-        print(f"About: {friend_data[5]}")
+        header(
+            f"{user_name[3]} {user_name[4]}'s Profile:\n")
 
-        # Fetch experience and education from their respective tables based on the user's ID
-        #cursor.execute("SELECT * FROM experience WHERE userID = ?", (globalVars.userID,))
-        cursor.execute("""
-            SELECT e.experienceID, e.userID, e.jobTitle, e.employer, e.dateStarted, e.dateEnded, e.location, e.description
-            FROM experience as e
-            JOIN users as u ON e.userID = u.userID 
-            WHERE e.userID = ?""", (friendID,))
-        experience_data = cursor.fetchall()
+        if friend_data:
+            print(f"Title: {friend_data[2]}")
+            print(f"Major: {friend_data[3]}")
+            print(f"University: {friend_data[4]}")
+            print(f"About: {friend_data[5]}")
 
-        if experience_data:
-            print("Experience:")
-            for exp in experience_data:
-                print("  - Job Title:", exp[2])
-                print("    Employer:", exp[3])
-                print("    Date Started:", exp[4])
-                print("    Date Ended:", exp[5])
-                print("    Location:", exp[6])
-                print("    Description:", exp[7])
+            # Fetch experience and education from their respective tables based on the user's ID
+            #cursor.execute("SELECT * FROM experience WHERE userID = ?", (globalVars.userID,))
+            cursor.execute("""
+                SELECT e.experienceID, e.userID, e.jobTitle, e.employer, e.dateStarted, e.dateEnded, e.location, e.description
+                FROM experience as e
+                JOIN users as u ON e.userID = u.userID 
+                WHERE e.userID = ?""", (friendID,))
+            experience_data = cursor.fetchall()
 
-        cursor.execute("""
-            SELECT edu.userID, edu.schoolName, edu.degree, edu.yearsAttended, u.userID
-            FROM education as edu
-            JOIN users as u ON edu.userID = u.userID 
-            WHERE edu.userID = ?""", (friendID,))
-        education_data = cursor.fetchall()
+            if experience_data:
+                print("Experience:")
+                for exp in experience_data:
+                    print("  - Job Title:", exp[2])
+                    print("    Employer:", exp[3])
+                    print("    Date Started:", exp[4])
+                    print("    Date Ended:", exp[5])
+                    print("    Location:", exp[6])
+                    print("    Description:", exp[7])
 
-        if education_data:
-            print("\nEducation:")
-            for edu in education_data:
-                print("  - School Name:", edu[1])
-                print("    Degree:", edu[2])
-                print("    Years Attended:", edu[3])
-    else:
-        print("\nFriend not found in the database.")
+            cursor.execute("""
+                SELECT edu.userID, edu.schoolName, edu.degree, edu.yearsAttended, u.userID
+                FROM education as edu
+                JOIN users as u ON edu.userID = u.userID 
+                WHERE edu.userID = ?""", (friendID,))
+            education_data = cursor.fetchall()
 
-    input("Press Enter to Continue...")
+            if education_data:
+                print("\nEducation:")
+                for edu in education_data:
+                    print("  - School Name:", edu[1])
+                    print("    Degree:", edu[2])
+                    print("    Years Attended:", edu[3])
+        else:
+            print("\nFriend not found in the database.")
+
+    except Exception as e:
+        print(f"Error occurred while fetching friends profiles: {e}")
+    #input("Press Enter to Continue...")
