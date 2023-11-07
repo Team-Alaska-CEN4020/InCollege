81e91ddb443c67f74fb40f6dc7873bbd2246b090
Jordan Archbell <jarchbell@usf.edu>
2023-11-07 10:05:52 -0500

    added notifications.py along with prototypes

---
+++ notifications.py
@@ -0,0 +1,27 @@
+def NotifyNeedToApply():
+    print("TODO: alerts at login that its been >7days since user's last application")
+    print("Remember - you're going to want to have a job when you graduate. Make sure that you start to apply for jobs today!")
+
+def NofifyNoProfile():
+    print("TODO: alerts at login that user has no profile")
+    print("Don't forget to create a profile")
+
+def NotifyUnreadMessage():
+    print("TODO: alert at login that user has an unread message")
+    print("You have messages waiting for you")
+
+def NotifyNewStudentJoin():
+    print("TODO: alert at login with a list of new student have joined since last login")
+    print("<first name> <last name> x has joined InCollege")
+
+def NotifyAppliedJobCount():
+    print("TODO: when in the jobs section, user sees below message with x replaced with actual number")
+    print("You have currently applied for x jobs")
+
+def NotifyNewJobPostings():
+    print("TODO: when in the jobs section, see a list of all new postings since last logged in")
+    print("A new job <job title> has been posted.")
+
+def NotifyJobDeleted():
+    print("TODO: when in the jobs section, user sees any jobs that have been deleted since last login")
+    print("A job that you applied for has been deleted <Job Title>")
